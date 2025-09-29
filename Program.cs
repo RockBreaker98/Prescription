efcad490362c52980b3a1003eb392aa788208385
RockBreaker98 <firasathhussaini4@gmail.com>
2025-09-28 22:44:28 -0500

    Create /home/data and run EF migrations on startup

--- Program.cs
+++ Program.cs
@@ -1,7 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using PrescriptionApp.Models;
 
-
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<PrescriptionContext>(options =>
@@ -12,17 +11,34 @@ builder.Services.Configure<RouteOptions>(o => o.LowercaseUrls = true);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+if (!builder.Environment.IsDevelopment())
+{
+    var home = Environment.GetEnvironmentVariable("HOME");   // Linux: /home ; Windows on App Service: D:\home
+    if (!string.IsNullOrWhiteSpace(home))
+    {
+        var dataDir = Path.Combine(home, "data");
+        Directory.CreateDirectory(dataDir);
+    }
+}
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<PrescriptionContext>();
+    db.Database.Migrate(); 
+}
+
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+
 // send every upper-case URL to lower-case
 app.Use(async (ctx, next) =>
 {
@@ -36,7 +52,6 @@ app.Use(async (ctx, next) =>
 });
 
 app.UseRouting();
-
 app.UseAuthorization();
 
 app.MapStaticAssets();
@@ -45,6 +60,5 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
-
 
 app.Run();
--- obj/Debug/net9.0/Prescription.AssemblyInfo.cs
+++ obj/Debug/net9.0/Prescription.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Prescription")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+215e4e192f6209b4f9808096a12b60818fdc65fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e33a1bb968f9ab313ee1be9d45603401edb089b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Prescription")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Prescription")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
